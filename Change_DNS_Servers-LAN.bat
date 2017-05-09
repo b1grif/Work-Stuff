e09c14bd245d7f0fe9d28f5a869361fe1a461038
Bill Griffiths <GriffithsB@mtl.corp>
2017-05-09 16:15:45 -0500

    Add Change_DNS_Server-LAN.bat

---
+++ Change_DNS_Servers-LAN.bat
@@ -0,0 +1,29 @@
+    @ECHO OFF
+REM ## Enter your IPv4 DNS Server IP addresses where indicated and run script with administrative privileges.
+    
+REM Set the variables    
+    set vardns1=172.16.0.5
+    set vardns2=172.16.0.6
+REM Prompt for the interface name
+    netsh int show interface
+    ECHO Type the interface name shown above
+    set /p int1=
+    
+REM Setting the primary DNS
+    ECHO Setting Primary DNS
+    netsh int ip set dns name = %int1% source = static addr = %vardns1%
+
+REM Setting the secondary DNS
+    ECHO Setting Secondary DNS
+    netsh int ip add dns name = %int1% addr = %vardns2%
+
+REM Flush DNS for good measure
+    ipconfig /flushdns
+
+REM Shows that the new DNS servers were set properly
+    ECHO Are the DNS servers set to %vardns1% and %vardns2%
+    ipconfig /all
+
+    pause
+
+    exit
